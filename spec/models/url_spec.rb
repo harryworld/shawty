9a26364268fd2dff087c0c300c78ca3c3a717a15
Harry Ng <harryworld@gmail.com>
2014-07-04 11:01:34 +0800

    Add more tests

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -8,4 +8,30 @@ RSpec.describe Url, :type => :model do
 
     expect(url).to be_valid
   end
+
+  it "will add http://" do
+    url = Url.new
+    url.original = 'www.google.com'
+    url.save
+
+    expect(url.original).to eq 'http://www.google.com'
+  end
+
+  it "is invalid if it does not contain .com" do
+    url = Url.new
+    url.original = 'generalassemb.ly'
+    url.save
+
+    expect(url).to be_invalid
+  end
+
+  it "is valid if it contains .com" do
+    url = Url.new
+    url.original = 'google.com'
+    url.save
+
+    expect(url).to be_valid
+  end
+
+  it "is invalid if the url does not start with http"
 end
